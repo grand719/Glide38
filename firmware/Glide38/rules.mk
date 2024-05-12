78ee8b04114f99eabb24c91c7a1b637f85c0252b
Łukasz Pawłowski <lukasz.adam.pawlowski@gmail.com>
2024-05-12 19:52:40 +0200

    first commit

---
+++ firmware/Glide38/rules.mk
@@ -0,0 +1,16 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+RGBLIGHT_ENABLE = no       # Enable underlight
+SPLIT_KEYBOARD = yes
+ENCODER_ENABLE = no
+OLED_ENABLE = no
+TAP_DANCE_ENABLE = yes
+LTO_ENABLE = yes
+CONVERT_TO = promicro_rp2040
